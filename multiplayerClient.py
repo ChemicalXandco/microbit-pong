21d15974bf7744d656ac9f5b327002af1bf4871a
ChemicalXandco <jimjoh41@gmail.com>
2020-08-26 16:36:21 +0100

    use derived classes for multiplayer

--- multiplayerClient.py
+++ multiplayerClient.py
@@ -3,6 +3,29 @@ import radio
 from game.core import *
 
 
+class ClientXY(XY):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def sync(self):
+        
+
+
+class ClientBall(Ball, ClientXY):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class ClientPaddle(Paddle, ClientXY):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
 class ClientPong(CorePong):
     def __init__(self, *args):
         super.__init__(*args)
+
+        self.ball = ClientBall(2, 2)
+
+        self.topPaddle = ClientPaddle(2, 0)
+        self.bottomPaddle = ClientPaddle(1, 4)
--- multiplayerHost.py
+++ multiplayerHost.py
@@ -3,6 +3,29 @@ import radio
 from game.core import *
 
 
+class HostXY(XY):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def sync(self):
+        
+
+
+class HostBall(Ball, HostXY):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class HostPaddle(Paddle, HostXY):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
 class HostPong(CorePong):
     def __init__(self, *args):
         super.__init__(*args)
+
+        self.ball = HostBall(2, 2)
+
+        self.topPaddle = HostPaddle(2, 0)
+        self.bottomPaddle = HostPaddle(1, 4)
